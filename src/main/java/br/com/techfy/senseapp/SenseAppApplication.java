0afa8ce636deeb07e948899f67167bef4351a7e8
murillocg <murillo.cg@gmail.com>
2018-07-13 13:38:11 -0300

    Application created with all modules needed

---
+++ src/main/java/br/com/techfy/senseapp/SenseAppApplication.java
@@ -0,0 +1,12 @@
+package br.com.techfy.senseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenseAppApplication.class, args);
+	}
+}
